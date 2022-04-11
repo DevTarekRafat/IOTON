1301495a3bbfee130d2737cd18cf266d5387aac9
Tarek Abdulbary <tarek.abdulbary97@gmail.com>
2022-04-11 11:45:45 +0200

    initial commit

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Footer } from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import "./Landing.css";
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <Header />
+      <h2>content</h2>
+      <Footer />
+    </section>
+  );
+};
+
+export default Landing;
