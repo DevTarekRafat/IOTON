3ba4a4d74749b10c72a1856b06350c5ef2fc33f9
Tarek Abdulbary <tarek.abdulbary97@gmail.com>
2022-04-12 15:15:34 +0200

    modify-hero-section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,118 @@
 import React from "react";
+import { ReactComponent as Logo } from "../../icons/logo.svg";
+import "./Header.css";
 
 const Header = () => {
-  return <h1>header</h1>;
+  return (
+    <nav>
+      <div className="container">
+        <div className="f-between">
+          <div id="logo">
+            <Logo />
+          </div>
+          <div className="links-container">
+            <ul className="links">
+              <li className="link-item">
+                <a className="link" href="">
+                  Home
+                </a>
+              </li>
+
+              <li className="link-item">
+                <a
+                  className="link dropdown-toggle"
+                  href="#"
+                  id="navbarScrollingDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Solutions
+                </a>
+
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="navbarScrollingDropdown"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="link-item">
+                <a
+                  className="link dropdown-toggle"
+                  href="#"
+                  id="navbarScrollingDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  About
+                </a>
+
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="navbarScrollingDropdown"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="link-item">
+                <a className="link" href="">
+                  Blog
+                </a>
+              </li>
+
+              <li className="link-item">
+                <a className="link" href="">
+                  Contact
+                </a>
+              </li>
+            </ul>
+            <div className="group-btn fg-between">
+              <button className="support-btn">Support</button>
+              <button className="login-btn">login</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -7,7 +7,36 @@ const Landing = () => {
   return (
     <section className="landing">
       <Header />
-      <h2>content</h2>
+      <section className="hero-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-12">
+              <div className="hero-content">
+                <span className="internet-txt">Internet of Things</span>
+                <h1>
+                  Save, Advance,
+                  <br /> Succeed with us!
+                </h1>
+                <p>
+                  We create highly-sophisticates software{" "}
+                  <span className="primary-txt"> integrated systems</span>{" "}
+                  through customized{" "}
+                  <span className="primary-txt"> IOT devices</span> , process
+                  datas with advanced algorithms and present these datas{" "}
+                  <span className="primary-txt">simplified</span> through
+                  customized through modern user friendly dashboard’s.
+                </p>
+                <button className="explore-btn">explore more</button>
+              </div>
+            </div>
+            <div className="col-md-6 col-12">
+              <div className="f-end">
+                <img src="/imgs/computing.png" alt="" className="img-fluid" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <Footer />
     </section>
   );
