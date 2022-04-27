3c4636ac400647a8c24571a58ba3e9f1a93981e1
Tarek Abdulbary <tarek.abdulbary97@gmail.com>
2022-04-27 15:30:13 +0200

    edit-footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 import "./Footer.css";
+import { ReactComponent as Logo } from "../../icons/logo.svg";
+import { ReactComponent as Facebook } from "../../icons/Facebook.svg";
+import { ReactComponent as Twitter } from "../../icons/Twitter.svg";
+import { ReactComponent as Linkedin } from "../../icons/Linkedin.svg";
+import { ReactComponent as Instagram } from "../../icons/instagram.svg";
+import { ReactComponent as Location } from "../../icons/location.svg";
+import { ReactComponent as Email } from "../../icons/email.svg";
 
 export const Footer = () => {
   return (
     <footer className="text-center text-lg-start">
       <div className="container p-4">
         <div className="row">
-          <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+          <div className="col-lg-5 col-md-12 mb-4 mb-md-0">
+            <div id="logo">
+              <Logo />
+            </div>
             <br />
             <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
-              atque ea quis molestias. Fugiat pariatur maxime quis culpa
-              corporis vitae repudiandae aliquam voluptatem veniam, est atque
-              cumque eum delectus sint!
+              We create highly-sophisticates software integrated systems through
+              customized IOT devices, process datas with advanced algorithms and
+              present these datas simplified through modern user friendly
+              dashboard’s.
             </p>
           </div>
 
           <div className="col-lg-2 col-md-6 mb-4 mb-md-0">
             <h5 className="text-uppercase">Company</h5>
+            <br />
 
             <ul className="list-unstyled mb-0">
               <li>
@@ -45,6 +56,7 @@ export const Footer = () => {
 
           <div className="col-lg-2 col-md-6 mb-4 mb-md-0">
             <h5 className="text-uppercase mb-0">Support</h5>
+            <br />
 
             <ul className="list-unstyled">
               <li>
@@ -80,23 +92,36 @@ export const Footer = () => {
             </ul>
           </div>
 
-          <div className="col-lg-2 col-md-6 mb-4 mb-md-0">
-            <h5 className="text-uppercase mb-0">Support</h5>
+          <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5 className="text-uppercase mb-0">Address</h5>
+            <br />
 
             <ul className="list-unstyled">
-              <li>
+              <li className="fg-between">
+                <Location />
                 <a href="#!" className="footer-link">
-                  27 Division St, New York,
+                  Kemp House, 160 City Road <br />
+                  London, EC1V 2NX
                 </a>
               </li>
-              <li>
+              <li className="fg-between">
+                <Email />
                 <a href="#!" className="footer-link">
-                  NY 10002, USA
+                  Email: hello@ioton.io
                 </a>
               </li>
-              <li>
-                <a href="#!" className="footer-link">
-                  Phone:+ (321) 984 754
+              <li className="social-media">
+                <a href="#">
+                  <Facebook />
+                </a>
+                <a href="#">
+                  <Twitter />
+                </a>
+                <a href="#">
+                  <Instagram />
+                </a>
+                <a href="#">
+                  <Linkedin />
                 </a>
               </li>
             </ul>
@@ -105,7 +130,7 @@ export const Footer = () => {
       </div>
 
       <div className="text-center p-3">
-        © 2020 Copyright:
+        © 2022 Copyright:
         <a className="footer-link">IOTON</a>
       </div>
     </footer>
